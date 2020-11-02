43435937909e3ffbeff6a7dc227c2335aceb5698
Adam Fuentes <66355715+Adam-Fuentes@users.noreply.github.com>
2020-11-02 10:16:10 +0100

    add random city

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));//código necesario para el body parser
+
 app.get("/", function(req, res){
 
-    const url = ("https://api.openweathermap.org/data/2.5/weather?q=Sabadell&units=metric&APPID=f18bc7b86643d6ffa4f93b6d87008e1b");
+    res.sendFile(__dirname+"/index.html")
+    
+});
+
+app.post("/", function(req, res){
+    //console.log(req.body.cityName);
+
+    const query = req.body.cityName;
+    const apiKey = "f18bc7b86643d6ffa4f93b6d87008e1b";
+    const unit = "metric";
+
+    const url = ("https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + unit + "&APPID=" + apiKey);
 
     https.get(url, function(response){
         console.log(response.statusCode);
@@ -23,7 +37,7 @@ app.get("/", function(req, res){
             console.log(weatherDescription);
             console.log(temp);
 
-            res.write("<h1>La temperatura en Sabadell es de: " + temp + " grados</h1>")
+            res.write("<h1>La temperatura en "+ query + " es de: " + temp + " grados</h1>")
             res.write("<p>El tiempo será " + weatherDescription + "</p>");
             res.write("<img src="+imgURL+"></img>");
             res.send();//si no se pone esto no acaba el proceso
@@ -34,3 +48,5 @@ app.get("/", function(req, res){
 app.listen(3000, function(){
     console.log("Server is running in port 3000");
 });
+/*
+*/
